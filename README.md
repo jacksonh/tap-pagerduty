88b8b2b525d89462964256c4ad4efadf529ccbb3
Jackson Harper <jackson@juvo.com>
2017-12-09 16:02:33 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# tap-pagerduty
+
+This is a [Singer](https://singer.io) tap that produces JSON-formatted data
+following the [Singer
+spec](https://github.com/singer-io/getting-started/blob/master/SPEC.md).
+
+This tap pulls raw incident data from the [PagerDuty REST API](https://v2.developer.pagerduty.com/v2/page/api-reference#!/Incidents/get_incidents) and extracts the
+following fields:
+
+- id
+- status
+- summary
+- html_url
+- last_status_change_by/summary
+- created_at
+- last_status_change_at'
+
+
+## Quick Start
+
+1. Create the config file
+
+   Create a JSON file called `config.json`. Its contents should look like:
+
+   ```json
+    {
+        "start_date": "2010-01-01",
+        "api_key": "<PagerDuty API Key>"
+    }
+    ```
+
+   The `start_date` specifies the date at which the tap will begin pulling data
+   (for those resources that support this).
+
+   The `api_key` is a `v2 Current` read-only API key from PagerDuty you can
+   create a PagerDuty API key here: https://juvomobile.pagerduty.com/api_keys
+
+2. Run the Tap in Discovery Mode
+
+    tap-pagerduty -c config.json -d
+
+   See the Singer docs on discovery mode
+   [here](https://github.com/singer-io/getting-started/blob/master/BEST_PRACTICES.md#discover-mode-and-connection-checks).
+
+3. Run the Tap in Sync Mode
+
+    tap-jira -c config.json
---
+++ tap_pagerduty.py
@@ -0,0 +1,54 @@
+
+import sys
+import json
+
+import pypd
+import singer
+from singer import utils
+
+REQUIRED_CONFIG_KEYS = ["start_date", "api_key"]
+
+def get_pagerduty_incidents(config):
+    res = []
+    pypd.api_key = config.get('api_key')
+    incidents = pypd.Incident.find(since=config.get('start_date'))
+    for incident in incidents:
+        res.append({
+                'id': str(incident.json['id']),
+                'status': str(incident.json['status']),
+                'summary': str(incident.json['summary']),
+                'url': str(incident.json['html_url']),
+                'last_status_change_by': str(incident.json['last_status_change_by']['summary']),
+                'created_at': str(incident.json['created_at']),
+                'last_updated_at': str(incident.json['last_status_change_at'])
+        })
+    return res
+
+def schema():
+    return {
+        'properties':   {
+            'id': {'type': 'string'},
+            'description': {'type': 'string'},
+            'status': {'type': 'string'},
+            'url': {'type': 'string'},
+            'last_status_change_by': {'type': 'string'},
+            'created_at': {'type': 'string', 'format': 'date-time'},
+            'last_updated_at': {'type': 'string', 'format': 'date-time'}
+            },
+        }
+
+def discover(config):
+    incidents = get_pagerduty_incidents(config)
+    singer.write_schema('pagerduty', schema(), 'id')
+
+
+def main():
+    args = utils.parse_args(REQUIRED_CONFIG_KEYS)
+    singer.write_schema('pagerduty', schema(), 'id')
+    if args.discover:
+        return
+    incidents = get_pagerduty_incidents(args.config)
+    singer.write_records('pagerduty', incidents)
+
+if __name__ == "__main__":
+    main()
